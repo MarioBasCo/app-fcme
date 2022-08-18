3e4200741c1c70267533df3ff8b7e2168b8ddf57
MarioBasCo <mario-basilio1998@hotmail.com>
2022-08-18 02:28:16 -0500

    disenio pagina pagos

--- src/app/pages/receipts/receipts.page.ts
+++ src/app/pages/receipts/receipts.page.ts
@@ -7,8 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReceiptsPage implements OnInit {
   transactions: any[] = [
-    {id: 1, vendor: 'Interes del primer mes', image: '', amount: 1500, time: '3:00PM'},
-    {id: 2, vendor: 'Pago del primer mes', image: '', amount: -1200, time: '4:00PM'}
+    {id: 1, vendor: 'Cuota N° 1', image: '33.33', amount: 4598.39, time: '401.61', state: 'T'},
+    {id: 2, vendor: 'Pago del primer mes', image: '', amount: -1200, time: '4:00PM', state: 'I'}
   ];
 
   constructor() { }
